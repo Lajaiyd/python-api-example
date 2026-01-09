6cce8fa65e1a1593ccd46633a58f23b38655e318
Olajide Akinyemi <iamjideakinyemi@gmail.com>
2026-01-09 16:11:45 -0500

    Increase dup factor

--- api_request.py
+++ api_request.py
@@ -1,10 +1,10 @@
 import requests
 
-base_url = "http://127.0.0.1:5000/process-text"
+base_url = "https://book-review-api-v7t4.onrender.com/process-text"
 
 params = {
     "text": "Hello World",
-    "duplication_factor": 3,
+    "duplication_factor": 30,
     "capitalization": "UPPER"
 }
 
